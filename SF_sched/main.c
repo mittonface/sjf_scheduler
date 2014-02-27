ad206f678a54f519b11e459595fda94e25de21ed
Brent Mitton <brent.mitton@gmail.com>
2014-02-27 18:53:14 -0400

    Initial Commit

---
+++ SF_sched/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SF_sched
+//
+//  Created by Brent Mitton on 2/27/2014.
+//  Copyright (c) 2014 Brent Mitton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
